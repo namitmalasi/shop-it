f96a66ea2b9494cd8b99e9259c38bd1fa6af5faf
Namit Malasi <namitmalasi@gmail.com>
2021-09-24 22:50:06 +0530

    making the site responsive

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import ProductList from "./pages/ProductList";
+// import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,11 @@ import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px" })}
 `;
 
 const Wrapper = styled.div`
@@ -12,6 +14,7 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
 `;
 const Left = styled.div`
   flex: 1;
@@ -21,6 +24,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
@@ -30,6 +34,7 @@ const SearchContainer = styled.div`
 `;
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
 `;
 const Center = styled.div`
   flex: 1;
@@ -37,16 +42,19 @@ const Center = styled.div`
 `;
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
 const Right = styled.div`
   flex: 1;
   display: flex;
   justify-content: flex-end;
+  ${mobile({ flex:2,justifyContent: "center" })}
 `;
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "12px",marginLeft:"10px"})}
 `;
 
 const Navbar = () => {
@@ -56,7 +64,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
