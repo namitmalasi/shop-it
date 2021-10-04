f30b6370b8bd876532679785ec60be978d57b39f
Namit Malasi <namitmalasi@gmail.com>
2021-10-04 20:55:29 +0530

    setup the server and database

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database Connected!!!");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port no: ${PORT}`);
+});
