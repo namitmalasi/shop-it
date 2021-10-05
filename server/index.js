1eee253ba10300cc1463dc87f8efa63aa3dd2896
Namit Malasi <namitmalasi@gmail.com>
2021-10-05 13:24:40 +0530

    created the mongoDB models

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
+app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
