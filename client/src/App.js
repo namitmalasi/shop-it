791d20570ec846b3780050cb95f70ea7ffc442af
Namit Malasi <namitmalasi@gmail.com>
2021-10-08 20:04:19 +0530

    added redux toolkit authentication

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,10 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
-  const user = true;
+  const user = useSelector((state) => state.user.currentUser);
   return (
     <Router>
       <Switch>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
+import { useState } from "react";
 import { mobile } from "../responsive";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../redux/apiCalls";
 
 const Container = styled.div`
   width: 100vw;
@@ -45,6 +48,10 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+  &:disabled {
+    color: green;
+    cursor: not-allowed;
+  }
 `;
 const Link = styled.a`
   margin: 5px 0px;
@@ -52,17 +59,39 @@ const Link = styled.a`
   text-decoration: underline;
   cursor: pointer;
 `;
-
+const Error = styled.span`
+  color: red;
+`;
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
+
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
+          <Input
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <Button>Sign In</Button>
+          <Button onClick={handleClick} disabled={isFetching}>
+            Sign In
+          </Button>
+          {error && <Error>Something went wrong...</Error>}
           <Link>Forgot Password?</Link>
           <Link>Create a new Account</Link>
         </Form>
